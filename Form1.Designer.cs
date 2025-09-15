142f33d9fd72b98408f9ccb38e5dd7138c65e09a
YENIFFER CHACON PARDO <30121210@tafe.wa.edu.au>
2025-09-15 12:11:39 +0800

    Initial commit-Astronomical Processing Sprint 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,12 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBoxSearch = new System.Windows.Forms.TextBox();
             this.labelSearch = new System.Windows.Forms.Label();
-            this.labelSort = new System.Windows.Forms.Label();
-            this.textBoxSort = new System.Windows.Forms.TextBox();
             this.textBoxEdite = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Edit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonLoad
@@ -45,26 +46,28 @@
             this.buttonLoad.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonLoad.ForeColor = System.Drawing.SystemColors.ControlText;
             this.buttonLoad.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.buttonLoad.Location = new System.Drawing.Point(540, 67);
+            this.buttonLoad.Location = new System.Drawing.Point(640, 36);
             this.buttonLoad.Name = "buttonLoad";
-            this.buttonLoad.Size = new System.Drawing.Size(80, 50);
+            this.buttonLoad.Size = new System.Drawing.Size(137, 69);
             this.buttonLoad.TabIndex = 0;
             this.buttonLoad.Text = "Load Date";
             this.buttonLoad.UseVisualStyleBackColor = false;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
             // 
             // listBox1
             // 
             this.listBox1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(540, 212);
+            this.listBox1.Location = new System.Drawing.Point(460, 126);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.Size = new System.Drawing.Size(226, 184);
             this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // textBoxSearch
             // 
-            this.textBoxSearch.Location = new System.Drawing.Point(141, 101);
+            this.textBoxSearch.Location = new System.Drawing.Point(211, 152);
             this.textBoxSearch.Name = "textBoxSearch";
             this.textBoxSearch.Size = new System.Drawing.Size(100, 20);
             this.textBoxSearch.TabIndex = 2;
@@ -73,42 +76,26 @@
             // 
             this.labelSearch.AutoSize = true;
             this.labelSearch.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSearch.Location = new System.Drawing.Point(84, 107);
+            this.labelSearch.Location = new System.Drawing.Point(116, 156);
             this.labelSearch.Name = "labelSearch";
             this.labelSearch.Size = new System.Drawing.Size(48, 16);
             this.labelSearch.TabIndex = 3;
             this.labelSearch.Text = "Search";
             this.labelSearch.Click += new System.EventHandler(this.label1_Click);
             // 
-            // labelSort
-            // 
-            this.labelSort.AutoSize = true;
-            this.labelSort.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSort.Location = new System.Drawing.Point(87, 170);
-            this.labelSort.Name = "labelSort";
-            this.labelSort.Size = new System.Drawing.Size(33, 16);
-            this.labelSort.TabIndex = 4;
-            this.labelSort.Text = "Sort";
-            // 
-            // textBoxSort
-            // 
-            this.textBoxSort.Location = new System.Drawing.Point(141, 170);
-            this.textBoxSort.Name = "textBoxSort";
-            this.textBoxSort.Size = new System.Drawing.Size(100, 20);
-            this.textBoxSort.TabIndex = 5;
-            // 
             // textBoxEdite
             // 
-            this.textBoxEdite.Location = new System.Drawing.Point(141, 256);
+            this.textBoxEdite.Location = new System.Drawing.Point(211, 259);
             this.textBoxEdite.Name = "textBoxEdite";
             this.textBoxEdite.Size = new System.Drawing.Size(100, 20);
             this.textBoxEdite.TabIndex = 6;
+            this.textBoxEdite.TextChanged += new System.EventHandler(this.textBoxEdite_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(84, 263);
+            this.label1.Location = new System.Drawing.Point(116, 263);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(31, 16);
             this.label1.TabIndex = 7;
@@ -124,16 +111,50 @@
             this.label2.TabIndex = 8;
             this.label2.Text = "Astronomical Processing";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(497, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(673, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 35);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Edit
+            // 
+            this.Edit.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Edit.Location = new System.Drawing.Point(357, 349);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(91, 34);
+            this.Edit.TabIndex = 11;
+            this.Edit.Text = "Edit";
+            this.Edit.UseVisualStyleBackColor = true;
+            this.Edit.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Edit);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxEdite);
-            this.Controls.Add(this.textBoxSort);
-            this.Controls.Add(this.labelSort);
             this.Controls.Add(this.labelSearch);
             this.Controls.Add(this.textBoxSearch);
             this.Controls.Add(this.listBox1);
@@ -151,11 +172,12 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBoxSearch;
         private System.Windows.Forms.Label labelSearch;
-        private System.Windows.Forms.Label labelSort;
-        private System.Windows.Forms.TextBox textBoxSort;
         private System.Windows.Forms.TextBox textBoxEdite;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Edit;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +14,49 @@ namespace neutrino_astronomy
 {
     public partial class Form1 : Form
     {
+        private readonly int[] dates = new int[max];
+        private const int max = 24;
+        private readonly Random rand = new Random();
+        private bool isLoaded=false;
+        private bool isSorted=false;
+        private int SelectedIndex = -1;
+
+
+
+
+        private void refreshlist()
+        {
+
+            listBox1.Items.Clear();
+            for (int i = 0; i < dates.Length; i++)
+            {
+                listBox1.Items.Add(dates[i]);
+            }
+
+        }
+
+        // This method is for Sort
+        // Fill
+        private void filArray()
+        {
+            for (int i = 0; i < dates.Length; i++)
+            {
+                dates[i] = rand.Next(10, 91);
+            }
+                
+            isLoaded = true;
+            isSorted = false;
+
+            refreshlist();
+            MessageBox.Show("Random numbers loaded between 10 and 91");
+
+
+
+        }
+
+
+
+
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +65,194 @@ namespace neutrino_astronomy
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedIndex = listBox1.SelectedIndex;
+            //Fill in the text box with the current value to edit.
+
+            if(SelectedIndex >= 0 && SelectedIndex < dates.Length)
+            {
+                textBoxEdite.Text = dates[SelectedIndex].ToString();
+            }
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            filArray();
+
+        }
+
+        
+
+        private void BubbleSortAnim()
+        {
+            
+            int n=dates.Length;
+            bool swapped;
+
+            do
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (dates[j] > dates[j + 1])
+                    {
+                        int tmp = dates[j];
+                        dates[j] = dates[j + 1];
+                        dates[j + 1] = tmp;
+                        swapped = true;
+                    }
+
+                    
+                   
+
+                }
+
+                n--;
+            }while(swapped);
+
+            refreshlist();
+            isSorted = true;
+
+        }
+
+        private void textBoxSort_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isLoaded)
+            {
+                MessageBox.Show("First load dates");
+                return;
+            }
+            BubbleSortAnim();
+            isSorted = true;
+            MessageBox.Show("Sorted data");
+        }
+
+
+
+
+        
+
+
+        private void ShowDates(int low, int high)
+        {
+            listBox1.Items.Clear();
+            for (int i=low; i < high; i++)
+            {
+                listBox1.Items.Add(dates[i]);
+            }
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!isLoaded)
+            {
+                MessageBox.Show("First load dates");
+                return;
+            }
+
+
+            //Show error if is empty
+            if (string.IsNullOrWhiteSpace(textBoxSearch.Text))
+            {
+                MessageBox.Show("\"Search box can't be empty\"");
+                return;
+            }
+            if (!int.TryParse(textBoxSearch.Text, out int target))
+            {
+                MessageBox.Show("You must enter a integer");
+                return;
+            }
+
+            //Ensuring order for binary search
+
+
+            int lowBound = 0;
+            int highBound = dates.Length - 1;
+
+            while (lowBound <= highBound)
+            {
+                int mid = (lowBound + highBound) / 2;
+                if (dates[mid] == target)
+                {
+                    //Select the index found in the list
+
+                    listBox1.SelectedIndex = mid;
+                    MessageBox.Show($"Search successful.Found at index {mid} with the value {dates[mid]}");
+                    
+                    return;
+
+
+                }
+                else if (dates[mid] > target)
+                {
+                    highBound = mid - 1;
+                }
+                else
+                {
+                    lowBound = mid + 1;
+                }
+
+                
+
+            }
+            MessageBox.Show("Search not sucessful.Try another value");
+        }
+
+
+
+
+
+            
+            
+               
+
+        
+
+        private void textBoxEdite_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!isLoaded)
+            {
+                MessageBox.Show("First load dates");
+                return;
+            }
+            
+            if (SelectedIndex < 0 || SelectedIndex >= dates.Length)
+            {
+                MessageBox.Show("You must select an index from the list");
+                return;
+            }
+            if (!int.TryParse(textBoxEdite.Text, out int newValue))
+            {
+                MessageBox.Show("You must enter an integer");
+                return;
+            }
+            if (newValue < 10 || newValue > 91)
+            {
+                MessageBox.Show("You must enter an integer between 10 and 91");
+                return;
+            }
+            dates[SelectedIndex] = newValue;
+            refreshlist();
+            listBox1.SelectedIndex = SelectedIndex;//Keep highlighted
+            MessageBox.Show("Value updated");
         }
     }
 }
